91869f30e79b5ecb28c0395cdc6c7b7e69ab1d66
uraloid <b310m0r@gmail.com>
2023-09-27 20:05:32 +0500

    Added output of selected patient information

--- main.js
+++ main.js
@@ -5,23 +5,74 @@ window.onload = () => {
   const inputDescription = document.querySelector("#inputDescription");
   const inputResult = document.querySelector("#inputResult");
 
-  fetchPatientData()
+  const patientSelect = document.querySelector("#patientSelect");
+  const patientEntry = document.querySelector("#patientEntry");
+  const researchEntry = document.querySelector("#researchEntry");
 
-  function renderPatientData(data) {
-    console.log(data)
+  patientSelect.addEventListener("change", (event) => onSelectChange(event));
+
+  fetchPatientData();
+
+  function choosePatient(patients) {
+    console.log(patients);
+
+    for (let i = 0; i < patients.length; i++) {
+      let optionElement = document.createElement("option");
+      optionElement.textContent = patients[i].fullName;
+      optionElement.value = patients[i].snils;
+
+      patientSelect.appendChild(optionElement);
+    }
   }
 
-  function fetchPatientData() {
-    fetch( 'patients.json' )
+  function onSelectChange(event) {
+    console.log(event.target.value);
+    const currentSnils = event.target.value;
+
+    fetch("patients.json")
       .then(function (response) {
         return response.json();
       })
       .then(function (data) {
-        renderPatientData(data);
+        const filteredPatient = data.patients.find(
+          (patient) => patient.snils === currentSnils
+        );
+        renderPatientData(filteredPatient);
       })
       .catch(function (error) {
         console.warn(error);
       });
   }
 
+  function renderPatientData(data) {
+    patientEntry.innerHTML = `<td>${data.fullName}</td>
+                               <td>${data.cardNumber}</td>
+                               <td>${data.snils}</td>
+                               <td>${data.address}</td>
+                               <td>${data.registrationResidence}</td>
+                               <td>${data.phoneNumber}</td>`;
+
+    const research = data.research[0];
+    researchEntry.innerHTML = `<td>${research.id}</td>
+                               <td>${research.name}</td>
+                               <td>${research.date}</td>
+                               <td>${research.description}</td>
+                               <td>${research.doctor}<td>
+                               <td>${research.result}<td>`;
+  }
+
+  function fetchPatientData() {
+    fetch("patients.json")
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        renderPatientData(data.patients[0]);
+        choosePatient(data.patients);
+        return data;
+      })
+      .catch(function (error) {
+        console.warn(error);
+      });
+  }
 };
